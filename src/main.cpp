90877212d930c3aa1f21a85bca450e59d1817c3b
Ali Karam <ak@alikaram.com>
2025-03-17 00:45:22 +0300

    fix stakeArm motor definition

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ pros::Imu imu(11);
 
 // motors
 pros::MotorGroup intake({12, -13}, pros::MotorGearset::green); // intake group - ports 8, 10 (reversed)
-pros::MotorGroup stakeArm({15}, pros::MotorGearset::red); // intake group - ports 8, 10 (reversed)
+pros::Motor stakeArm({15}, pros::MotorGearset::red); // intake group - ports 8, 10 (reversed)
 
 // pneumatics
 pros::adi::Pneumatics mogoHook('A', false); // hook pneumatics on ports A starting in the retracted position
@@ -217,8 +217,7 @@ void opcontrol() {
         }
 
         if (controller.get_digital(DIGITAL_X)) {
-            hook1.toggle();
-            hook2.toggle();
+            mogoHook.toggle();
             pros::delay(500);
         }
 
