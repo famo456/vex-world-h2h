90ed02b9d23ca332dc25647da8324f15835d210e
Ali Karam <ak@alikaram.com>
2025-04-02 13:49:17 +0300

    change intake lift to on press

--- src/main.cpp
+++ src/main.cpp
@@ -203,11 +203,12 @@ void opcontrol() {
         if (controller.get_digital(DIGITAL_L1)) {
              intake.move(-127);
              lift.move(-127);
-        }
-
-        if (controller.get_digital(DIGITAL_L2)) {
+        } else if (controller.get_digital(DIGITAL_L2)) {
             intake.move(127);
             lift.move(127);
+        } else {
+            intake.brake();
+            lift.brake();
         }
 
         if (controller.get_digital(DIGITAL_R1)) {
